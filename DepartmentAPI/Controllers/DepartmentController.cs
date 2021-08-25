0d0e9f35cc045f4e9b538711e996a114c5c930bf
Sebastian Škaper <sebastian.skaper@metronik.si>
2021-08-25 12:29:08 +0200

    Initial commit.

---
+++ DepartmentAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using DepartmentAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DepartmentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string sqlDataSource;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            sqlDataSource = _configuration.GetConnectionString("DepartmentConnectionString");
+        }
+
+        // GET: api/<DepartmentController>
+        [HttpGet]
+        public List<Department> Get()
+        {
+            string selectQuery = "SELECT id, [name] FROM dbo.department";
+            List<Department> departments = new List<Department>();
+
+            using (SqlConnection con = new SqlConnection(sqlDataSource))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(selectQuery, con))
+                {
+                    SqlDataReader sqlDataReader = cmd.ExecuteReader();
+
+                    while(sqlDataReader.Read())
+                    {
+                        Department dep = new Department();
+                        dep.Id = sqlDataReader.GetInt32("id");
+                        dep.Name = sqlDataReader.GetString("name");
+
+                        departments.Add(dep);
+                    }
+                }
+            }
+
+            return departments;
+        }
+
+        // GET api/<DepartmentController>/5
+        [HttpGet("{id}")]
+        public Department Get(int id)
+        {
+            string selectQuery = "SELECT id, [name] FROM dbo.department WHERE id = " + id;
+
+            Department dep = new Department();
+
+            using (SqlConnection con = new SqlConnection(sqlDataSource))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(selectQuery, con))
+                {
+                    SqlDataReader sqlDataReader = cmd.ExecuteReader();
+
+                    if (sqlDataReader.Read())
+                    {
+                        dep.Id = sqlDataReader.GetInt32("id");
+                        dep.Name = sqlDataReader.GetString("name");
+                    }
+                }
+            }
+
+            return dep;
+        }
+
+        // POST api/<DepartmentController>
+        [HttpPost]
+        public void Post(Department department)
+        {
+            string selectQuery = @"INSERT INTO dbo.department VALUES ('" + department.Name + @"')";
+
+            using (SqlConnection con = new SqlConnection(sqlDataSource))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(selectQuery, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // PUT api/<DepartmentController>/5
+        [HttpPut]
+        public void Put(Department department)
+        {
+            string selectQuery = @"UPDATE dbo.department SET name = '" + department.Name + @"' WHERE id = '" + department.Id + "'";
+
+            using (SqlConnection con = new SqlConnection(sqlDataSource))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(selectQuery, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // DELETE api/<DepartmentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            string selectQuery = @"DELETE FROM dbo.department WHERE id = '" + id + "'";
+
+            using (SqlConnection con = new SqlConnection(sqlDataSource))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(selectQuery, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
